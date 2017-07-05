3a7ff72e9ecd6acd7540318cf64f19b88a56c870
dart-wakar <wakar@whitelogic.in>
2017-07-05 14:37:20 +0530

    Getting and setting all 5 kinds of data for a particular movie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import {AppService} from './app.service';
+import {movieTitles} from './movieTitles';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +13,18 @@ export class AppComponent implements OnInit {
   constructor(private appService: AppService) {}
 
   ngOnInit() {
-
+    
   }
 
   gatherData() {
-    this.appService.getData()
+    this.appService.getBooksData()
         .subscribe(results => {
           console.log(results);
-          this.setMovieData(results);
+          this.setBooksData(results);
         })
   }
 
-  setMovieData(res: any) {
+  setBooksData(res: any) {
     var data:any = {};
     data.movie_id = 250;
     data.movie_name = res.Similar.Info[0].Name;
@@ -35,6 +36,112 @@ export class AppComponent implements OnInit {
     this.appService.setBooksData(data)
         .subscribe(movie => {
           console.log(movie.books);
+          this.getShowsData();
+        });
+  }
+
+  getShowsData() {
+    this.appService.getShowsData()
+      .subscribe(results => {
+        console.log(results);
+        this.setShowsData(results);
+      });
+  }
+
+  setShowsData(res: any) {
+    var data:any = {};
+    data.movie_id = 250;
+    data.movie_name = res.Similar.Info[0].Name;
+    data.movie_wikipedia_description = res.Similar.Info[0].wTeaser;
+    data.movie_wikipedia_url = res.Similar.Info[0].wUrl;
+    data.movie_youtube_url = res.Similar.Info[0].yUrl;
+    data.movie_youtube_id = res.Similar.Info[0].yID;
+    data.shows = res.Similar.Results;
+    this.appService.setShowsData(data)
+        .subscribe(movie => {
+          console.log(movie.books);
+          console.log(movie.shows);
+          this.getMusicData();
+        });
+  }
+
+  getMusicData() {
+    this.appService.getMusicData()
+      .subscribe(results => {
+        console.log(results);
+        this.setMusicData(results);
+      });
+  }
+
+  setMusicData(res: any) {
+    var data:any = {};
+    data.movie_id = 250;
+    data.movie_name = res.Similar.Info[0].Name;
+    data.movie_wikipedia_description = res.Similar.Info[0].wTeaser;
+    data.movie_wikipedia_url = res.Similar.Info[0].wUrl;
+    data.movie_youtube_url = res.Similar.Info[0].yUrl;
+    data.movie_youtube_id = res.Similar.Info[0].yID;
+    data.music = res.Similar.Results;
+    this.appService.setMusicData(data)
+        .subscribe(movie => {
+          console.log(movie.books);
+          console.log(movie.shows);
+          console.log(movie.music);
+          this.getAuthorsData();
+        });
+  }
+
+  getAuthorsData() {
+    this.appService.getAuthorsData()
+      .subscribe(results => {
+        console.log(results);
+        this.setAuthorsData(results);
+      });
+  }
+
+  setAuthorsData(res: any) {
+    var data:any = {};
+    data.movie_id = 250;
+    data.movie_name = res.Similar.Info[0].Name;
+    data.movie_wikipedia_description = res.Similar.Info[0].wTeaser;
+    data.movie_wikipedia_url = res.Similar.Info[0].wUrl;
+    data.movie_youtube_url = res.Similar.Info[0].yUrl;
+    data.movie_youtube_id = res.Similar.Info[0].yID;
+    data.authors = res.Similar.Results;
+    this.appService.setAuthorsData(data)
+        .subscribe(movie => {
+          console.log(movie.books);
+          console.log(movie.shows);
+          console.log(movie.music);
+          console.log(movie.authors);
+          this.getGamesData();
+        });
+  }
+
+  getGamesData() {
+    this.appService.getGamesData()
+      .subscribe(results => {
+        console.log(results);
+        this.setGamesData(results);
+      });
+  }
+
+  setGamesData(res: any) {
+    var data:any = {};
+    data.movie_id = 250;
+    data.movie_name = res.Similar.Info[0].Name;
+    data.movie_wikipedia_description = res.Similar.Info[0].wTeaser;
+    data.movie_wikipedia_url = res.Similar.Info[0].wUrl;
+    data.movie_youtube_url = res.Similar.Info[0].yUrl;
+    data.movie_youtube_id = res.Similar.Info[0].yID;
+    data.games = res.Similar.Results;
+    this.appService.setGamesData(data)
+        .subscribe(movie => {
+          console.log(movie.books);
+          console.log(movie.shows);
+          console.log(movie.music);
+          console.log(movie.authors);
+          console.log(movie.games);
         });
   }
 
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,7 +10,7 @@ export class AppService {
 
     private accessKey = '275418-TellMe-RYYV1TBH';
 
-    getData(): Observable<any> {
+    getBooksData(): Observable<any> {
         return this.http.get('https://tastedive.com/api/similar?q=harry potter&k=275418-TellMe-RYYV1TBH&limit=20&info=1&type=books')
                         .map((res) => res.json())
                         .catch((err) => {
@@ -21,6 +21,78 @@ export class AppService {
 
     setBooksData(booksData: any): Observable<any> {
         return this.http.post('http://127.0.0.1:3003/api/moviessimilar/books',booksData)
+                .map((res) => res.json())
+                .catch((err) => {
+                    console.log(err);
+                    return Observable.throw(err.json().error || 'Server Error');
+                });
+    }
+
+    getShowsData(): Observable<any> {
+        return this.http.get('https://tastedive.com/api/similar?q=harry potter&k=275418-TellMe-RYYV1TBH&limit=20&info=1&type=shows')
+                        .map((res) => res.json())
+                        .catch((err) => {
+                            console.log(err);
+                            return Observable.throw(err.json().error || 'Server Error');
+                        });
+    }
+
+    getMusicData(): Observable<any> {
+        return this.http.get('https://tastedive.com/api/similar?q=harry potter&k=275418-TellMe-RYYV1TBH&limit=20&info=1&type=music')
+                        .map((res) => res.json())
+                        .catch((err) => {
+                            console.log(err);
+                            return Observable.throw(err.json().error || 'Server Error');
+                        });
+    }
+
+    getAuthorsData(): Observable<any> {
+        return this.http.get('https://tastedive.com/api/similar?q=harry potter&k=275418-TellMe-RYYV1TBH&limit=20&info=1&type=authors')
+                        .map((res) => res.json())
+                        .catch((err) => {
+                            console.log(err);
+                            return Observable.throw(err.json().error || 'Server Error');
+                        });
+    }
+
+    getGamesData(): Observable<any> {
+        return this.http.get('https://tastedive.com/api/similar?q=harry potter&k=275418-TellMe-RYYV1TBH&limit=20&info=1&type=games')
+                        .map((res) => res.json())
+                        .catch((err) => {
+                            console.log(err);
+                            return Observable.throw(err.json().error || 'Server Error');
+                        });
+    }
+
+    setShowsData(showsData: any): Observable<any> {
+        return this.http.post('http://127.0.0.1:3003/api/moviessimilar/shows',showsData)
+                .map((res) => res.json())
+                .catch((err) => {
+                    console.log(err);
+                    return Observable.throw(err.json().error || 'Server Error');
+                });
+    }
+
+    setMusicData(musicData: any): Observable<any> {
+        return this.http.post('http://127.0.0.1:3003/api/moviessimilar/music',musicData)
+                .map((res) => res.json())
+                .catch((err) => {
+                    console.log(err);
+                    return Observable.throw(err.json().error || 'Server Error');
+                });
+    }
+
+    setAuthorsData(authorsData: any): Observable<any> {
+        return this.http.post('http://127.0.0.1:3003/api/moviessimilar/authors',authorsData)
+                .map((res) => res.json())
+                .catch((err) => {
+                    console.log(err);
+                    return Observable.throw(err.json().error || 'Server Error');
+                });
+    }
+
+    setGamesData(gamesData: any): Observable<any> {
+        return this.http.post('http://127.0.0.1:3003/api/moviessimilar/games',gamesData)
                 .map((res) => res.json())
                 .catch((err) => {
                     console.log(err);
